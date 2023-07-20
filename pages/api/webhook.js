65e86129b295a12630b49ddb1543b93653646518
HASSAN AHMED <hassaansultan720@gmail.com>
2023-07-20 11:28:32 +0500

    Fully funtional Frontend

---
+++ pages/api/webhook.js
@@ -0,0 +1,50 @@
+import { mongooseConnect } from "@/lib/mongoose";
+const stripe = require("stripe")(process.env.STRIPE_SK);
+import { buffer } from "micro";
+import { Order } from "@/models/Order";
+
+endpoint_secret = 'whsec_d3e14470cc211b5f0d64406bc131e14980514ec1f637bb3ef74dc5d9d868bf50'
+
+
+export default async function handler(req, res) {
+  await mongooseConnect();
+  const sig = req.headers["stripe-signature"];
+
+  let event;
+
+  try {
+    event = stripe.webhooks.constructEvent(
+      await buffer(req),
+      sig,
+      endpointSecret
+    );
+  } catch (err) {
+    res.status(400).send(`Webhook Error: ${err.message}`);
+    return;
+  }
+
+  // Handle the event
+  switch (event.type) {
+    case "checkout.session.completed":
+      const data = event.data.object;
+      const orderId = data.metadata.orderId;
+      const paid = data.payment_status === "paid";
+      if (orderId && paid) {
+        await Order.findByIdAndUpdate(orderId, {
+          paid: true,
+        });
+      }
+      break;
+    default:
+      console.log(`Unhandled event type ${event.type}`);
+  }
+
+  res.status(200).send("ok");
+}
+
+export const config = {
+  api: { bodyParser: false },
+};
+
+// bright-thrift-cajole-lean
+// acct_1Lj5ADIUXXMmgk2a
