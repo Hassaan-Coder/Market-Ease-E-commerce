83bfc5cb7876763c06e2cd0d2b341b3f142d41ab
HASSAN AHMED <hassaansultan720@gmail.com>
2023-07-20 11:38:59 +0500

    Fully funtional Frontend

--- components/SingleOrder.js
+++ components/SingleOrder.js
@@ -7,37 +7,41 @@ const StyledOrder = styled.div`
   display: flex;
   gap: 20px;
   align-items: center;
-  time{
+  time {
     font-size: 1rem;
     color: #555;
   }
 `;
 const ProductRow = styled.div`
-  span{
-    color:#aaa;
+  span {
+    color: #aaa;
   }
 `;
 const Address = styled.div`
-  font-size:.8rem;
-  line-height:1rem;
+  font-size: 0.8rem;
+  line-height: 1rem;
   margin-top: 5px;
-  color:#888;
+  color: #888;
 `;
 
-export default function SingleOrder({line_items,createdAt,...rest}) {
+export default function SingleOrder({ line_items, createdAt, ...rest }) {
   return (
     <StyledOrder>
       <div>
-        <time>{(new Date(createdAt)).toLocaleString('sv-SE')}</time>
+        <time>{new Date(createdAt).toLocaleString("sv-SE")}</time>
         <Address>
-          {rest.name}<br />
-          {rest.email}<br />
-          {rest.streetAddress}<br />
+          {rest.name}
+          <br />
+          {rest.email}
+          <br />
+          {rest.streetAddress}
+          <br />
           {rest.postalCode} {rest.city}, {rest.country}
         </Address>
       </div>
       <div>
-        {line_items.map(item => (
+        {line_items.map((item) => (
+          // eslint-disable-next-line react/jsx-key
           <ProductRow>
             <span>{item.quantity} x </span>
             {item.price_data.product_data.name}
